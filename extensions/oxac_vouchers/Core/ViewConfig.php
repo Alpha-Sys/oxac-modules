7be29e81efb50cfce8805827117dfef7ac015b98
Fabian Kunkler <fabian.kunkler@alpha-sys.de>
2018-01-26 11:38:13 +0100

    voucher module

--- extensions/oxac_vouchers/Core/ViewConfig.php
+++ extensions/oxac_vouchers/Core/ViewConfig.php
@@ -5,6 +5,16 @@ namespace OxidAcademy\VoucherModule\Core;
 class ViewConfig extends ViewConfig_parent{
 
     public function getShowVouchers(){
-        return false;
+        $oBasket = $this->getSession()->getBasket();
+        $dBrutto = $oBasket->getBruttoSum();
+
+        $oConfig = $this->getConfig();
+        $iSetting = $oConfig->getConfigParam('sOxacVoucherBasketValue');
+
+        if($iSetting < $dBrutto){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
