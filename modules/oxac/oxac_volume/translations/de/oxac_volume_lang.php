a37f631069677a9913d6754ddcf8c19db7cdcbe7
Fabian Kunkler <fabian.kunkler@alpha-sys.de>
2018-03-04 15:49:30 +0100

    oxac volume language vars

---
+++ modules/oxac/oxac_volume/translations/de/oxac_volume_lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * OXID Academy Volume Module
+ */
+
+$sLangName  =   'Deutsch';
+$aLang      =   array(
+    'charset'                               =>  'UTF-8',
+    'OXAC_VOLUME'                           =>  'Volumen',
+);
--- modules/oxac/oxac_volume/views/blocks/details_productmain_artnumber.tpl
+++ modules/oxac/oxac_volume/views/blocks/details_productmain_artnumber.tpl
@@ -1,5 +1,5 @@
 [{$smarty.block.parent}]
-
-<b>[{$oDetailsProduct->getVolume()}]</b>
+<br>
+[{oxmultilang ident="OXAC_VOLUME" suffix="COLON"}] <b>[{$oDetailsProduct->getVolume()}]</b>
 
 [{oxstyle include="css/styles.min.css"}]
